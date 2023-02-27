e208de8b2df840fbe1f42e4383fb81883f53a49a
jiachunyuan <chunyuan.jia@mongodb.com>
2023-02-27 15:42:45 +0800

    modify fromAccountId and toAccountId in method generateTransfer

--- src/main/java/com/mongodb/test/service/AccountService.java
+++ src/main/java/com/mongodb/test/service/AccountService.java
@@ -207,12 +207,4 @@ public class AccountService {
         return transfers;
     }
 
-    public static void main(String[] args) {
-        for(int i=0; i< 100 ;i++){
-            System.out.println(">>>>>>>>" + ((int) Math.floor(Math.random() * 6) + 1) * 100000000);
-
-        }
-
-
-    }
 }
