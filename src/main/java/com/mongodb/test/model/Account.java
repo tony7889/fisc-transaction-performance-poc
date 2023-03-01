b59a91fe3c6d272cdf6b03b46ec52b423b1ccb3e
tony7889 <tony.lcw@gmail.com>
2023-03-01 15:58:18 +0800

    Update Account.java
    Added getter

--- src/main/java/com/mongodb/test/model/Account.java
+++ src/main/java/com/mongodb/test/model/Account.java
@@ -3,6 +3,7 @@ package com.mongodb.test.model;
 import org.bson.codecs.pojo.annotations.BsonId;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -11,7 +12,7 @@ public class Account {
     @BsonId
     private int id;
     //private ObjectId id;
-    private double balance;
+    @Getter private double balance;
     public Account(int id, double balance) {
         this.id = id;
         this.balance = balance;
